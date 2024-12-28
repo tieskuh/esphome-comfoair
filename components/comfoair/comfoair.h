af5bf98bbe6b79d602d374ee7b1e783e5f419213
bammetje <40342759+bammetje@users.noreply.github.com>
2024-12-28 18:42:55 +0100

    Update comfoair.h

--- components/comfoair/comfoair.h
+++ components/comfoair/comfoair.h
@@ -284,7 +284,7 @@ protected:
       // checksum is without checksum bytes
       uint8_t checksum = comfoair_checksum_(this->data_ + 2, COMFOAIR_MSG_HEAD_LENGTH + data_length - 2);
       if (checksum != byte) {
-        // ESP_LOGW(TAG, "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X", this->data_[0], this->data_[1], this->data_[2], this->data_[3], this->data_[4], this->data_[5], this->data_[6], this->data_[7], this->data_[8], this->data_[9], this->data_[10],this->data_[11],this->data_[12],this->data_[13],this->data_[14]);
+        ESP_LOGW(TAG, "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X", this->data_[0], this->data_[1], this->data_[2], this->data_[3], this->data_[4], this->data_[5], this->data_[6], this->data_[7], this->data_[8], this->data_[9], this->data_[10],this->data_[11],this->data_[12],this->data_[13],this->data_[14]);
         ESP_LOGW(TAG, "ComfoAir Checksum klopt niet: 0x%02X!=0x%02X", byte, checksum);
         return false;
       }
